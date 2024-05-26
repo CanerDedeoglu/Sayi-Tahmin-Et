81cc3d3b6f03d301216e898a1948f7990103fb1d
Caner Dedeoğlu <070caner4@gmail.com>
2024-05-26 15:46:09 +0300

    write code oyunEkrani and sonucEkrani

--- app/src/main/java/com/canerdedeoglu/sayitahminet/OyunEkraniFragment.kt
+++ app/src/main/java/com/canerdedeoglu/sayitahminet/OyunEkraniFragment.kt
@@ -1,29 +1,64 @@
 package com.canerdedeoglu.sayitahminet
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.canerdedeoglu.sayitahminet.databinding.FragmentOyunEkraniBinding
 
-
 class OyunEkraniFragment : Fragment() {
 
+    private var hak = 5
+    private val sayi = (1..100).random()
     lateinit var viewBinding: FragmentOyunEkraniBinding
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
         viewBinding = FragmentOyunEkraniBinding.inflate(inflater, container, false)
 
+        Log.d("sayi", "tahminEt: $sayi")
+
         viewBinding.btnTahmin.setOnClickListener {
-
-            Navigation.findNavController(it).navigate(R.id.sonucEkranıGecis)
+            tahminEt()
         }
-
 
         return viewBinding.root
     }
 
+    private fun tahminEt() {
+        val sayiGir = viewBinding.txtSayiGir.text.toString().toIntOrNull()
+
+        if (sayiGir == null) {
+            Toast.makeText(requireContext(), "Lütfen sayı giriniz!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val sonuc = sayiGir == sayi
+
+        if (sonuc) {
+            Log.d("sonuc", "true")
+            val action = OyunEkraniFragmentDirections.sonucEkranıGecis(true)
+            Navigation.findNavController(viewBinding.root).navigate(action)
+        } else {
+            Log.d("sonuc", "false")
+            hak--
+
+            if (hak > 0) {
+                if (sayiGir < sayi) {
+                    viewBinding.txtIslem.text = "Sayıyı Arttır!"
+                } else {
+                    viewBinding.txtIslem.text = "Sayıyı Azalt!"
+                }
+                viewBinding.txtHak.text = "Kalan Hakkınız: $hak"
+            } else {
+                val action = OyunEkraniFragmentDirections.sonucEkranıGecis(false)
+                Navigation.findNavController(viewBinding.root).navigate(action)
+            }
+        }
+    }
 }
--- app/src/main/java/com/canerdedeoglu/sayitahminet/SonucEkraniFragment.kt
+++ app/src/main/java/com/canerdedeoglu/sayitahminet/SonucEkraniFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 import com.canerdedeoglu.sayitahminet.databinding.FragmentSonucEkraniBinding
 
 
 class SonucEkraniFragment : Fragment() {
+
 
     lateinit var viewBinding: FragmentSonucEkraniBinding
 
@@ -17,11 +19,30 @@ class SonucEkraniFragment : Fragment() {
 
         viewBinding = FragmentSonucEkraniBinding.inflate(inflater, container, false)
 
+        SonucEkrani()
+
         viewBinding.btnTekrar.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.anaSayfaGidis)
         }
 
         return viewBinding.root
+    }
+
+    fun SonucEkrani()
+    {
+        val bundle : SonucEkraniFragmentArgs by navArgs()
+        val gelenSonuc = bundle.sonuc
+
+        if(gelenSonuc == true)
+        {
+            viewBinding.imgView.setImageResource(R.drawable.ic_happy)
+            viewBinding.txtSonuc.text = "Tebrikler bildiniz!"
+        }
+        else
+        {
+            viewBinding.imgView.setImageResource(R.drawable.ic_sad)
+            viewBinding.txtSonuc.text = "Üzgünüz kaybettiniz!"
+        }
     }
 
 
